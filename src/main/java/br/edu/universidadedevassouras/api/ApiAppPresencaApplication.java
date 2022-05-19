c5c4e629753edf6de67c8ada2a362f3b9dcc893e
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-05-19 00:33:49 -0300

    Initial commit

---
+++ src/main/java/br/edu/universidadedevassouras/api/ApiAppPresencaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.universidadedevassouras.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAppPresencaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAppPresencaApplication.class, args);
+    }
+
+}
